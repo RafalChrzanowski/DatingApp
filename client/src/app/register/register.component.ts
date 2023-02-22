efe8e2d64de80d9bbecca54eaf6e6a9ba8948d75
Kszanu <rafalchrzanowski00@gmail.com>
2023-02-22 20:46:54 +0100

    fin 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -27,7 +27,7 @@ export class RegisterComponent implements OnInit {
     this.registerForm = this.fb.group({
       gender: ['male'],
       username: ['', Validators.required],
-      knowAs: ['', Validators.required],
+      knownAs: ['', Validators.required],
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
